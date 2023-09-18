d993c0b3383353efbcaec3150f935bb469147caf
danielred007 <danielrojo1927@gmail.com>
2023-09-18 21:09:11 +0100

    custom btns with custom props

--- full-react-theme/src/theme.ts
+++ full-react-theme/src/theme.ts
@@ -1,18 +1,33 @@
 // src/theme.js
 import { createTheme } from '@mui/material/styles';
 
+declare module '@mui/material/styles' {
+  interface PaletteOptions {
+    tertiary?: {
+      main: string;
+    };
+  }
+}
+
 const theme = createTheme({
   palette: {
+    mode: 'dark',
     primary: {
       main: '#1976D2'
     },
     secondary: {
       main: '#FF4081'
     },
+    tertiary: {
+      main: '#FF4081'
+    },
   },
   typography: {
     fontFamily: 'Roboto, Arial, sans-serif',
-  }
+  },
+  shape: {
+    borderRadius: 0,
+  },
 });
 
 export default theme;
